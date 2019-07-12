41825049ddfb5a4f010bb55e597c7407ba5472fb
Nguyen Quyen <cuonbinhthuan@gmail.com>
2019-07-13 01:04:25 +0700

    add nav

--- javascript/toggleButton.js
+++
@@ -1,6 +0,0 @@
-function toggleButton() {
-    const menus = document.querySelectorAll('.list-group-item');
-    menus.forEach(menu => menu.classList.toggle('.toggle-button-show'));
-}
-document.querySelector('.toggle-button').addEventListener('click', toggleButton);
-alert('sdf')
